5558dd0920b8c2af2a059bc61e12f24eb3950d55
Md. Aasduzzaman Rana <iamtheasad@gmail.com>
2021-09-27 18:53:32 +0600

    working on dropzone

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -140,69 +140,123 @@ function treeMenu() {
 treeMenu();
 
 
-// Dropzone All Kind of File Ulploader
+/* Dropzone All Kind of File Ulploader */
 
-if ($("#all-files-uploder").length) {
-    Dropzone.autoDiscover = false;
-    var dropzone = new Dropzone('#all-files-uploder', {
-        previewTemplate: document.querySelector('.preview-template').innerHTML,
-        parallelUploads: 2,
-        thumbnailHeight: 120,
-        thumbnailWidth: 120,
-        maxFilesize: 3,
-        filesizeBase: 1000,
-        thumbnail: function (file, dataUrl) {
-            if (file.previewElement) {
-                file.previewElement.classList.remove("dz-file-preview");
-                var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
-                for (var i = 0; i < images.length; i++) {
-                    var thumbnailElement = images[i];
-                    thumbnailElement.alt = file.name;
-                    thumbnailElement.src = dataUrl;
-                }
-                setTimeout(function () { file.previewElement.classList.add("dz-image-preview"); }, 1);
-            }
-        }
+
+
+if ($(".all-files-uploder").length) {
+
+    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+    var previewNode = document.querySelector("#template");
+    previewNode.id = "";
+    var previewTemplate = previewNode.parentNode.innerHTML;
+    previewNode.parentNode.removeChild(previewNode);
+
+    var myDropzone = new Dropzone(".all-files-uploder", { // Make the whole body a dropzone
+        url: "/upload", // Set the url
+        thumbnailWidth: 80,
+        thumbnailHeight: 80,
+        parallelUploads: 20,
+        previewTemplate: previewTemplate,
+        autoQueue: false, // Make sure the files aren't queued until manually added
+        previewsContainer: ".previews", // Define the all-files-uploder to display the previews
+        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
     });
 
-    var minSteps = 6,
-        maxSteps = 60,
-        timeBetweenSteps = 100,
-        bytesPerStep = 100000;
+    myDropzone.on("addedfile", function (file) {
+        // Hookup the start button
+        file.previewElement.querySelector(".start").onclick = function () { myDropzone.enqueueFile(file); };
+    });
 
-    dropzone.uploadFiles = function (files) {
-        console.log(files);
-        var self = this;
+    // Update the total progress bar
+    myDropzone.on("totaluploadprogress", function (progress) {
+        document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+    });
 
-        for (var i = 0; i < files.length; i++) {
+    myDropzone.on("sending", function (file) {
+        // Show the total progress bar when upload starts
+        document.querySelector("#total-progress").style.opacity = "1";
+        // And disable the start button
+        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+    });
 
-            var file = files[i];
-            totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+    // Hide the total progress bar when nothing's uploading anymore
+    myDropzone.on("queuecomplete", function (progress) {
+        document.querySelector("#total-progress").style.opacity = "0";
+    });
 
-            for (var step = 0; step < totalSteps; step++) {
-                var duration = timeBetweenSteps * (step + 1);
-                setTimeout(function (file, totalSteps, step) {
-                    return function () {
-                        file.upload = {
-                            progress: 100 * (step + 1) / totalSteps,
-                            total: file.size,
-                            bytesSent: (step + 1) * file.size / totalSteps
-                        };
-
-                        self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
-                        if (file.upload.progress == 100) {
-                            file.status = Dropzone.SUCCESS;
-                            self.emit("success", file, 'success', null);
-                            self.emit("complete", file);
-                            self.processQueue();
-                            document.getElementsByClassName("dz-success-mark").style.opacity = "1";
-                        }
-                    };
-                }(file, totalSteps, step), duration);
-            }
-        }
-    }
+    // Setup the buttons for all transfers
+    // The "add files" button doesn't need to be setup because the config
+    // `clickable` has already been specified.
+    document.querySelector("#actions .start").onclick = function () {
+        myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+    };
+    document.querySelector("#actions .cancel").onclick = function () {
+        myDropzone.removeAllFiles(true);
+    };
 }
+
+// if ($("#all-files-uploder").length) {
+//     Dropzone.autoDiscover = false;
+//     var dropzone = new Dropzone('#all-files-uploder', {
+//         previewTemplate: document.querySelector('.preview-template').innerHTML,
+//         parallelUploads: 2,
+//         thumbnailHeight: 120,
+//         thumbnailWidth: 120,
+//         maxFilesize: 3,
+//         filesizeBase: 1000,
+//         thumbnail: function (file, dataUrl) {
+//             if (file.previewElement) {
+//                 file.previewElement.classList.remove("dz-file-preview");
+//                 var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+//                 for (var i = 0; i < images.length; i++) {
+//                     var thumbnailElement = images[i];
+//                     thumbnailElement.alt = file.name;
+//                     thumbnailElement.src = dataUrl;
+//                 }
+//                 setTimeout(function () { file.previewElement.classList.add("dz-image-preview"); }, 1);
+//             }
+//         }
+//     });
+
+//     var minSteps = 6,
+//         maxSteps = 60,
+//         timeBetweenSteps = 100,
+//         bytesPerStep = 100000;
+
+//     dropzone.uploadFiles = function (files) {
+//         console.log(files);
+//         var self = this;
+
+//         for (var i = 0; i < files.length; i++) {
+
+//             var file = files[i];
+//             totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+//             for (var step = 0; step < totalSteps; step++) {
+//                 var duration = timeBetweenSteps * (step + 1);
+//                 setTimeout(function (file, totalSteps, step) {
+//                     return function () {
+//                         file.upload = {
+//                             progress: 100 * (step + 1) / totalSteps,
+//                             total: file.size,
+//                             bytesSent: (step + 1) * file.size / totalSteps
+//                         };
+
+//                         self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+//                         if (file.upload.progress == 100) {
+//                             file.status = Dropzone.SUCCESS;
+//                             self.emit("success", file, 'success', null);
+//                             self.emit("complete", file);
+//                             self.processQueue();
+//                             document.getElementsByClassName("dz-success-mark").style.opacity = "1";
+//                         }
+//                     };
+//                 }(file, totalSteps, step), duration);
+//             }
+//         }
+//     }
+// }
 
 
 
