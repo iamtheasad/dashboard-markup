8f18daf0a395f60f44a60018a481df5aeb8914b8
Md. Aasduzzaman Rana <iamtheasad@gmail.com>
2021-09-27 14:25:51 +0600

    dropzone added

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -140,6 +140,72 @@ function treeMenu() {
 treeMenu();
 
 
+// Dropzone All Kind of File Ulploader
+
+if ($("#all-files-uploder").length) {
+    Dropzone.autoDiscover = false;
+    var dropzone = new Dropzone('#all-files-uploder', {
+        previewTemplate: document.querySelector('.preview-template').innerHTML,
+        parallelUploads: 2,
+        thumbnailHeight: 120,
+        thumbnailWidth: 120,
+        maxFilesize: 3,
+        filesizeBase: 1000,
+        thumbnail: function (file, dataUrl) {
+            if (file.previewElement) {
+                file.previewElement.classList.remove("dz-file-preview");
+                var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+                for (var i = 0; i < images.length; i++) {
+                    var thumbnailElement = images[i];
+                    thumbnailElement.alt = file.name;
+                    thumbnailElement.src = dataUrl;
+                }
+                setTimeout(function () { file.previewElement.classList.add("dz-image-preview"); }, 1);
+            }
+        }
+    });
+
+    var minSteps = 6,
+        maxSteps = 60,
+        timeBetweenSteps = 100,
+        bytesPerStep = 100000;
+
+    dropzone.uploadFiles = function (files) {
+        console.log(files);
+        var self = this;
+
+        for (var i = 0; i < files.length; i++) {
+
+            var file = files[i];
+            totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+            for (var step = 0; step < totalSteps; step++) {
+                var duration = timeBetweenSteps * (step + 1);
+                setTimeout(function (file, totalSteps, step) {
+                    return function () {
+                        file.upload = {
+                            progress: 100 * (step + 1) / totalSteps,
+                            total: file.size,
+                            bytesSent: (step + 1) * file.size / totalSteps
+                        };
+
+                        self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+                        if (file.upload.progress == 100) {
+                            file.status = Dropzone.SUCCESS;
+                            self.emit("success", file, 'success', null);
+                            self.emit("complete", file);
+                            self.processQueue();
+                            document.getElementsByClassName("dz-success-mark").style.opacity = "1";
+                        }
+                    };
+                }(file, totalSteps, step), duration);
+            }
+        }
+    }
+}
+
+
+
 (function ($) {
     "use strict";
 
@@ -435,6 +501,7 @@ treeMenu();
         }
     }
 
+
     /*==========================================================================
         WHEN DOCUMENT LOADING
     ==========================================================================*/
@@ -501,3 +568,4 @@ treeMenu();
     });
 
 })(window.jQuery);
+
