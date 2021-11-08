8e306198abe3750d883febef35edfcf2184fca1d
Md. Aasduzzaman Rana <iamtheasad@gmail.com>
2021-11-08 17:22:09 +0600

    refactor: dateRange start to end script updated

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -111,15 +111,90 @@ function rangeSlider() {
 // Date Picker
 function datePicker() {
     if (window.daterangepicker) {
-        $('.duration-date-activator').daterangepicker({
-            opens: 'left',
+
+        // var nowDate = new Date();
+        // var today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
+
+        // $('.single-date-activator').daterangepicker({
+        //     opens: 'right',
+        //     "autoApply": true,
+        //     "singleDatePicker": true,
+        //     locale: {
+        //         format: 'DD MMMM, YYYY',
+        //     },
+        //     "startDate": today,
+        //     // "endDate": "12/20/2021",
+        //     parentEl: ".campaign-wrapper",
+        // },
+        //     // function (start, end, label) {
+        //     //     console.log('New date range selected: ' + start.format('DD MMMM, YYYY') + ' to ' + end.format('DD MMMM, YYYY') + ' (predefined range: ' + label + ')');
+        //     // }
+        // );
+
+        var nowDate = new Date();
+        var today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
+        var maxLimitDate = new Date(nowDate.getFullYear() + 1, nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
+        var minDateForSecondPicker;
+
+        $('.single-date-activator').daterangepicker({
             "autoApply": true,
+            // "autoUpdateInput": false,
             "singleDatePicker": true,
-            "startDate": "09/14/2021",
-            "endDate": "09/20/2021",
+            "minDate": today,
+            "maxDate": maxLimitDate,
+            "opens": "left",
+            "locale": {
+                format: 'DD MMMM, YYYY'
+            },
             parentEl: ".campaign-wrapper",
-        }, function (start, end, label) {
-            console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+        }, function (start, end) {
+            $(".single-date-activator").val(start.format('DD MMMM, YYYY'));
+            $('.single-date-activator').parent().parent().removeClass('has-error');
+            console.log($(".single-date-activator").val());
+        }
+        )
+    };
+
+
+    if (window.daterangepicker) {
+        var nowDate = new Date();
+        var today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
+        var maxLimitDate = new Date(nowDate.getFullYear() + 1, nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
+        var minDateForSecondPicker;
+
+        $('.duration-date-activator-start').daterangepicker({
+            "autoApply": true,
+            // "autoUpdateInput": false,
+            "singleDatePicker": true,
+            "minDate": today,
+            "maxDate": maxLimitDate,
+            "opens": "left",
+            "locale": {
+                format: 'DD MMMM, YYYY'
+            },
+            parentEl: ".campaign-wrapper",
+        }, function (start, end) {
+            $(".duration-date-activator-start").val(start.format('DD MMMM, YYYY'));
+            $('.duration-date-activator-start').parent().parent().removeClass('has-error');
+            console.log($(".duration-date-activator-start").val());
+
+            var aMinDate = new Date(Date.parse(start));
+
+            $('.duration-date-activator-end').daterangepicker({
+                "autoApply": true,
+                // "autoUpdateInput": false,
+                "singleDatePicker": true,
+                "minDate": aMinDate,
+                "maxDate": maxLimitDate,
+                "opens": "left",
+                "locale": {
+                    format: 'DD MMMM, YYYY'
+                },
+                parentEl: ".campaign-wrapper",
+            }, function (start, end) {
+                $(".duration-date-activator-end").val(start.format('DD MMMM, YYYY'));
+                $('.duration-date-activator-end').parent().parent().removeClass('has-error');
+            });
         });
     }
 }
@@ -237,14 +312,12 @@ $('.registration-form').on('submit', function (e) {
 });
 
 
-
 (function ($) {
     "use strict";
 
     /*==========================================================================
         :: All Essential Functions
     ==========================================================================*/
-
 
     // Dropdown Search with select2
     function dropdownSearchNselect() {
